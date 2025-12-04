354c6db6cab3ca737b8618a0d37bb0c8503fd4dd
gh0st1608 <egalindoa@uni.pe>
2025-12-04 12:40:41 -0500

    fix

--- dist/domain/repository/appointment.repository.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RDSAppointmentSymbol = exports.DynamoAppointmentSymbol = void 0;
-exports.DynamoAppointmentSymbol = Symbol('IDynamoAppointmentRepository');
-exports.RDSAppointmentSymbol = Symbol('IRDSAppointmentRepository');
--- dist/domain/repository/event.repository.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EBAppointmentEventPublisherSymbol = exports.SNSAppointmentEventPublisherSymbol = void 0;
-exports.SNSAppointmentEventPublisherSymbol = Symbol('ISNSAppointmentEventPublisher');
-exports.EBAppointmentEventPublisherSymbol = Symbol('IEBAppointmentEventPublisher');
